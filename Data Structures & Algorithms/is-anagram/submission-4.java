e221221a1c9ce6f9a517c52e1fcd8cb67726e664
Hititrips <tripathihiti75@gmail.com>
2026-07-07 22:22:30 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+  if(s.length()!=t.length()){
+    return false;
+  }
+       HashMap<Character,Integer> h = new HashMap<>();
+        for(char c: s.toCharArray()){
+            h.put(c,h.getOrDefault(c,0)+1);
+           
+        }
+        for(char c : t.toCharArray()){
+            if(!h.containsKey(c) || h.get(c)==0){
+                return false;
+            }
+           
+            h.put(c,h.get(c)-1);
+
+        }
+       
+       return true;
+
+    }
+}
